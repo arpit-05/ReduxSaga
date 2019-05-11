504795df5f0de74ae9f6f5868b3861c33d749e17
Arpit Trikha <arpit.trikha@gmail.com>
2019-05-12 02:42:36 +0530

    Adding Images Saga

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import imagesReducer from './imagesReducer';
 import loadingReducer from './loadingReducers';
 import errorReducer from './errorReducer';
+import pageReducer from './pageReducer';
 const rootReducer = combineReducers({
     isLoading: loadingReducer,
     images: imagesReducer,
     error: errorReducer,
+    nextPage: pageReducer,
 });
 export default rootReducer;
